430be341b59cccfb4888d5789d5ceb5ddf1ef66b
Katy Molony <85511799+k8molony@users.noreply.github.com>
2023-04-26 11:27:25 -0400

    removed dist from gitignore

--- src/components/series-view/series-view.jsx
+++ src/components/series-view/series-view.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import './series-view.scss';
-import { Container, Card, Button, Row } from 'react-bootstrap';
+import PropTypes from "prop-types";
+import "./series-view.scss";
+import { Container, Card, Button, Row } from "react-bootstrap";
 
 export class SeriesView extends React.Component {
-
   render() {
     const { series, onBackClick, movies } = this.props;
 
@@ -22,17 +21,26 @@ export class SeriesView extends React.Component {
               <span className="value">{series.Description}</span>
             </Card.Text>
 
-            <Button variant="dark" onClick={() => { onBackClick(); }}>Back</Button>
+            <Button
+              variant="dark"
+              onClick={() => {
+                onBackClick();
+              }}
+            >
+              Back
+            </Button>
           </Card.Body>
         </Card>
         <Row>
-          {movies.map(movie => (
-            <Card className="favorite-movie card-content" key={movie._id} >
-              <Card.Img className="fav-poster" variant="top" src={movie.ImagePath} />
+          {movies.map((movie) => (
+            <Card className="favorite-movie card-content" key={movie._id}>
+              <Card.Img
+                className="fav-poster"
+                variant="top"
+                src={movie.ImagePath}
+              />
               <Card.Body style={{ backgroundColor: "#90adc6" }}>
-                <Card.Title className="movie_title">
-                  {movie.Title}
-                </Card.Title>
+                <Card.Title className="movie_title">{movie.Title}</Card.Title>
               </Card.Body>
             </Card>
           ))}
